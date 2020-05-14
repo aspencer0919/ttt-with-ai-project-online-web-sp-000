19021fb242f093865f62e6524eea4e7065acb000
Adam Spencer <aspencer0919@gmail.com>
2020-05-14 16:21:24 +0000

    Automatically backed up by Learn

---
+++ lib/game.rb
@@ -0,0 +1,45 @@
+class Game
+
+  WIN_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ]
+
+  def won?
+    a = WIN_COMBINATIONS.find{
+      |combo|
+      @board[combo[0]] == "X" && @board[combo[1]] == "X" && @board[combo[2]] == "X"
+    }
+    b = WIN_COMBINATIONS.find{
+      |combo|
+      @board[combo[0]] == "O" && @board[combo[1]] == "O" && @board[combo[2]] == "O"
+    }
+    return a || b
+  end
+
+  def draw?
+    !won? && full?
+  end
+
+  def over?
+    if won? || draw? == true
+      return true
+    else
+      return false
+    end
+  end
+
+  def winner
+    if won?
+      @board[won?[0]] == "X" ? "X" : "O"
+    else
+      nil
+    end
+  end
+end
