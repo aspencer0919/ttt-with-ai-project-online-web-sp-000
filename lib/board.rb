c6bbff51b99e7c6fff3ec6640cbc91010a7fc437
Adam Spencer <aspencer0919@gmail.com>
2020-05-14 16:27:26 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,17 +1,25 @@
 class Board
 
-  def initialize(board = nil)
-    @board = board || Array.new(9, " ")
-    @input = nil
-  end
-
-  def cells(move)
-
-  end
+  @cells = []
 
   def reset!
-    @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+    self.cells = Array.new(9, " ")
   end
+
+  
+  def initialize
+    self.cells = Array.new(9, " ")
+  end
+
+  def display
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+    puts " "
+  end
+
 
 
 end
